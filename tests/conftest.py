6c1df7039e6d20517777bdd87d5bac42eab93fc4
David Yair <Davismarsel@gmail.com>
2021-11-24 14:02:31 +0200

    Added '--no-sandbox' passing to driver init options

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ import datetime
 def chrome_options(chrome_options):
     chrome_options.add_argument('headless')
     chrome_options.add_argument('disable-gpu')
+    chrome_options.add_argument('no-sandbox')
     chrome_options.add_argument('window-size=1200x600')
     return chrome_options
 
